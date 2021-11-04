d3471fdb6dccb273392769874a783357f692043f
dm-marina <mysummer1404@gmail.com>
2021-11-04 15:06:40 +0200

    dm-marina

--- js/script.js
+++ js/script.js
@@ -1,79 +1,60 @@
 const age=prompt("Введи свой возраст:");
 const city = prompt("В каком городе ты живешь?");
 const sport = prompt("Напиши свой любимый вид спорта:")
+let userAge;
+let country;
+let sportsmenName;
 switch(age){
     case age:
-        {
-            let userAge
-        }
         userAge = `Твой возраст ${age}`
         alert(userAge);
         break;
     case '':
-        {
-            let userAge
-        }
         userAge = 'Жаль, что ты не зохотел ввести свой возраст'
         alert(userAge);
         break;
 }
 // age >=0 ? `Твой возраст ${age}` : 'Жаль, что ты не зохотел ввести свой возраст'
 switch(city){
-    case 'Киев':{
-        let country
-    }
+    case 'Киев':
         country = 'Ты живешь в столице Украины';
         alert(country);
         break;
-    case 'Москва':{
-        let country
-    }
+    case 'Москва':
         country = 'Ты живешь в столице Росии';
         alert(country);
         break;
-    case 'Минск':{
-        let country
-    }
+    case 'Минск':
         country = 'Ты живешь в столице Беларуси';
         alert(country);
         break;
-    case '':{let country}
+    case '':
         country='Жаль, что ты не зохотел ввести свой город';
         alert(country);
         break
-    default:{let country}
+    default:
         country =`Ты живешь в городе ${city}`
         alert(country);
         break
 }
 switch(sport){
-    case 'бокс':{
-        let sportsmenName
-    }
+    case 'бокс':
     sportsmenName='Круто! Хочешь стать Майком Тайсоном?'
     alert(sportsmenName);
     break;
-    case 'футбол':{
-        let sportsmenName
-    }
+    case 'футбол':
     sportsmenName='Круто! Хочешь стать Криштиану Роналду?'
     alert(sportsmenName);
     break;
-    case 'плавание':{
-        let sportsmenName
-    }
+    case 'плавание':
     sportsmenName='Круто! Хочешь стать Мэттью Бионди?'
     alert(sportsmenName);
     break
-    case '':{
-        let sportsmenName
-    }
+    case '':
     sportsmenName= 'Жаль, что ты не зохотел ввести любимый вид спорта'
     alert(sportsmenName);
     break;
-    default:{
-        let sportsmenName
-    }
+    default:
     sportsmenName = `Твой любимый вид спорта это ${sport}`
     alert(sportsmenName);
 }
